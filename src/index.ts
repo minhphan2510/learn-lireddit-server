b49eaaa0bfcfc75941c7aaec0ae156abb5f183a3
Minh Phan <minh.phan@enouvo.com>
2020-10-13 22:59:27 +0700

    config project

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+
+const main = async () => {
+	const orm = await MikroORM.init({
+		entities: [Post],
+		dbName: 'lireddit',
+		type: 'postgresql',
+		debug: !__prod__
+	});
+
+	const post = orm.em.create(Post, { title: 'My first post' });
+	await orm.em.persistAndFlush(post);
+	await orm.em.nativeInsert(Post, { title: 'My second post' });
+};
+
+main().catch((error) => {
+	console.error(error);
+});
